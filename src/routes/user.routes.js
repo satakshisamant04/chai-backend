fb1cd483f335bd230449b2a0ba6e203304389c47
Satakshi Samant <satakshisamant04@gmail.com>
2025-10-31 10:52:03 +0530

    fix: correct multer config and file upload middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js';
-import {upload} from '../middlewares/fileUpload.middleware.js';
+import { upload } from '../middlewares/multer.middleware.js';
+
 const router = Router();
 
-router.post('/register', registerUser);
-upload.fields([
-    {
-        name: 'avatar', maxCount: 1
-    },
-    {
-        name: 'coverImage', maxCount: 5
-    }
-])
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
+
 export default router;
